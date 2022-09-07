0828cfcbbb1ddced5b8e8a7ade53a014507b8cdd
Connor Allen <m.connor.allen@gmail.com>
2022-09-07 06:24:53 -0400

    progress update

--- routes/html-routes/index.js
+++ routes/html-routes/index.js
@@ -1,7 +1,12 @@
+const path = require('path');
 const router = require('express').Router();
-const { notes } = require ('/Code/Note_taker/Develop/db/db.json');
+
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
   });
+
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/notes.html'));
+}); 
   module.exports = router;
--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/api-routes');
+const htmlRoutes = require('./routes/html-routes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+// apiRoutes
+
+app.use('/', htmlRoutes);
 
 
 app.listen(PORT, () => {
